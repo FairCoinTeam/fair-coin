c18187d96cf01aeed98718373cbab2273b1c2a3e
thokon00 <tom@fair-coin.org>
2014-06-05 23:07:21 +0200

    FIXED: QRCODE support can now be compiled in

--- src/qt/qtipcserver.h
+++ src/qt/qtipcserver.h
@@ -1,8 +1,8 @@
 #ifndef QTIPCSERVER_H
 #define QTIPCSERVER_H
 
-// Define Bitcoin-Qt message queue name
-#define BITCOINURI_QUEUE_NAME "BitcoinURI"
+// Define Faircoin-Qt message queue name
+#define FAIRCOINURI_QUEUE_NAME "FaircoinURI"
 
 void ipcScanRelay(int argc, char *argv[]);
 void ipcInit(int argc, char *argv[]);
