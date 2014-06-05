fe98d014fb370654a999e11096ea13a443eb4426
thokon00 <tom@fair-coin.org>
2014-06-05 23:05:19 +0200

    updated version information (v1.0.1.1)

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,15 +1,27 @@
 #ifndef CLIENTVERSION_H
 #define CLIENTVERSION_H
 
+#if defined(HAVE_CONFIG_H)
+#include "faircoin-config.h"
+#else
 //
-// client versioning
+// client versioning and copyright year
 //
 
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       1
 #define CLIENT_VERSION_MINOR       0
-#define CLIENT_VERSION_REVISION    0
+#define CLIENT_VERSION_REVISION    1
 #define CLIENT_VERSION_BUILD       1
+
+// Set to true for release, false for prerelease or test build
+#define CLIENT_VERSION_IS_RELEASE  false
+
+// Copyright year (2009-this)
+// Todo: update this when changing our copyright comments in the source
+#define COPYRIGHT_YEAR 2014
+
+#endif //HAVE_CONFIG_H
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
