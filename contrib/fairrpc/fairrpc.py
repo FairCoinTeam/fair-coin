a5d11d703f63749e0032fae1bcce81af85c39705
thokon00 <tom@fair-coin.org>
2015-02-26 10:39:37 +0100

    UPDATED: bitrpc -> fairrpc

--- contrib/fairrpc/fairrpc.py
+++ contrib/fairrpc/fairrpc.py
@@ -10,9 +10,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-	access = ServiceProxy("http://127.0.0.1:8332")
+	access = ServiceProxy("http://127.0.0.1:46393")
 else:
-	access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:8332")
+	access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:46393")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
